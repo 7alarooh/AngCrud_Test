7dd9a1836cbce62baf06fb8ab85fa2168ea26268
7alarooh <azzaalmahrooqi79@gmail.com>
2023-09-12 16:42:46 +0400

    first commit

---
+++ src/app/fruits/fruits.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FruitsRoutingModule } from './fruits-routing.module';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FruitsRoutingModule,
+    
+  ]
+})
+export class FruitsModule { }
